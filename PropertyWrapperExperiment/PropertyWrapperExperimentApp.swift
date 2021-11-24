79676a05e72858de3d5ce02f50f6f4cdfa9e0131
İlkay AKTAŞ <iaktas@aselsan.com.tr>
2021-11-24 09:11:29 +0300

    Initial Commit

---
+++ PropertyWrapperExperiment/PropertyWrapperExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropertyWrapperExperimentApp.swift
+//  PropertyWrapperExperiment
+//
+//  Created by İlkay Aktaş on 24.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct PropertyWrapperExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
